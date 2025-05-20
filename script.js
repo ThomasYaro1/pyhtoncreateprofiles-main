43861d6493d631b792465eeb2562254bfa9c730c
Ditt Namn <yarothomas@gmail.com>
2025-05-20 11:16:31 +0200

    Fix table data population in script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    const list = document.getElementById('list');
-    list.innerHTML = '';
+    const tbody = document.querySelector('#allItems tbody');
+    tbody.innerHTML = '';
     data.forEach(customer => {
-      list.innerHTML += `<p>${customer.Givenname} ${customer.Surname} - ${customer.TelephoneCountryCode} ${customer.Telephone}</p>`;
+      const row = `
+        <tr>
+          <td>${customer.Givenname} ${customer.Surname}</td>
+          <td>${customer.Country}</td>
+          <td>${customer.Birthday.split('-')[0]}</td>
+        </tr>
+      `;
+      tbody.innerHTML += row;
     });
-  });
+  })
+  .catch(error => console.error('Error loading JSON:', error));
