cfa6c979373b807faf2a0f51cffb8d0eedc73d8c
Ditt Namn <yarothomas@gmail.com>
2025-05-20 11:24:55 +0200

    Fix JSON format and script update

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-    const tbody = document.querySelector('#allItems tbody');
-    tbody.innerHTML = '';
-    data.forEach(customer => {
-      const row = `
-        <tr>
-          <td>${customer.Givenname} ${customer.Surname}</td>
-          <td>${customer.Country}</td>
-          <td>${customer.Birthday.split('-')[0]}</td>
-        </tr>
-      `;
-      tbody.innerHTML += row;
+const allMoviesTBody = document.querySelector("#allItems tbody")
+
+const showTable = function(moviesArray){
+    allMoviesTBody.innerHTML = ""
+    for(let i = 0; i < moviesArray.length;i++) { 
+        let trText = `<tr><th scope="row">${moviesArray[i].Givenname} ${moviesArray[i].Surname}</th><td>${moviesArray[i].Country}</td><td>${moviesArray[i].Birthday.substring(0,4)}</td></tr>`
+        allMoviesTBody.innerHTML += trText
+    }
+
+} 
+
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => {
+        showTable(json)
     });
-  })
-  .catch(error => console.error('Error loading JSON:', error));
